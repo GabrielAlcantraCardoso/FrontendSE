8dfd7a635659dd2b49e30f3d4a3c7e3d980078f6
Gabriel Alcantra Cardoso <gabriel.fiel.ajesus@gmail.com>
2022-12-06 22:13:58 +0100

    Sign up componentt

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {  BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from'./components/pages/Home';
+import Auth from './components/Auth';
 
 function App() {
 
   return (
     <div>
   
-      
       <Router>
         <Navbar/>
         <Switch>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Auth}/>
         </Switch>
       </Router>
       
